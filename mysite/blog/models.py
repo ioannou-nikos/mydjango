232c52f27dab83909bf7cf82a08d92e0d963f4a3
Nikos Ioannou <ioannou.nikos@gmail.com>
2020-06-09 22:03:51 +0300

    Mail, Comments, Tagging (ch 2) DONE

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from taggit.managers import TaggableManager # For the tagging system
 
 # Custom Model Manager
 class PublishedManager(models.Manager):
@@ -41,4 +41,20 @@ class Post (models.Model):
                               self.publish.day, self.slug])
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    tags = TaggableManager()
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, 
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updates = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
